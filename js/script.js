e925aa7dcd040351ff8985180f4d707a57eb999b
Fredrik Welander <fredrik.welander@subsite.fi>
2021-04-20 19:21:13 +0300

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('load');
+
+var jsonObj = { name: "Bo", age: 5, toys: ["car", "doll"]};
+
+$(function() {
+
+
+
+    $.ajax({
+        type: "POST",
+        url: "https://fw-teaching.fi/demo/wdbocms/api/v1/products/", 
+        data: JSON.stringify({ name : "Gloves", price: 24.90 }),
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+            listProducts();
+        },
+    });
+});
+
+
+function listProducts() {
+    $.ajax({
+        type: "GET",
+        url: "https://fw-teaching.fi/demo/wdbocms/api/v1/products/", 
+        success: function(data) {
+            console.log(data);
+        },
+    });
+}
