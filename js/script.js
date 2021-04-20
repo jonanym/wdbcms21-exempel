0af95ec24f793dc5a95e55a3138ff0cb07f55935
Fredrik Welander <welandfr@xena.arcada.fi>
2021-04-20 20:37:59 +0300

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 
-$(function() {
+$(document).ready(function () {
+    
+    getIp();
 
-    console.log("load");
 });
+
+var foo = { name: "Bo", age: 5 };
+
+function getIp() {
+
+    $.get("https://cgi.arcada.fi/~welandfr/demo/wdbcms21-exempel/api/ip/", function(data) {
+        console.log(data.ip);
+        
+        $("#ip").html(data.ip);
+    });
+
+    
+}
